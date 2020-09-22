4f26ee9b49157be2a3d1dd429d899b4865a9411c
Ryan N <ryannguyentt@gmail.com>
2020-09-22 22:19:22 +1000

    Create stateful header

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,41 @@ import PropTypes from "prop-types"
 import React from "react"
 import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img width="40" src={require('../images/logo-wandrlens.svg')} /></Link>
-      <Link to="/stories">Stories</Link>
-      <Link to="/galleries">Galleries</Link>
-      <Link to="/buy"><button>Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor (props) {
+      super();
+
+      this.state = {
+        hasScrolled: false
+      }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (e) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img width="40" src={require('../images/logo-wandrlens.svg')} /></Link>
+          <Link to="/stories">Stories</Link>
+          <Link to="/galleries">Galleries</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
